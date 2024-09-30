cec8a2acb67d949ca19aa5f4e61bacc520c1c7e1
ahonkazi <aohinuzzaman420@gmail.com>
2024-09-30 10:44:55 +0600

    updated course details page

--- src/pages/dashboard/course-details/components/course-modules/CourseModuleList.jsx
+++ src/pages/dashboard/course-details/components/course-modules/CourseModuleList.jsx
@@ -3,11 +3,15 @@ import { SectionTitleDashboard } from "../../../../../components/typography/Typo
 import { IoPlayCircleOutline } from "react-icons/io5";
 import { MdOutlineAssignmentLate } from "react-icons/md";
 import ModuleCard from "../../../../../components/card/ModuleCard";
+import { ButtonPrimary } from "../../../../../components/button/Button";
 
 const CourseModuleList = () => {
   return (
     <div className="mt-content">
-      <SectionTitleDashboard>Course modules</SectionTitleDashboard>
+      <div className="flex items-center justify-between">
+        <SectionTitleDashboard>Course modules</SectionTitleDashboard>
+        <ButtonPrimary>Add new module</ButtonPrimary>
+      </div>
       <div className="mt-content">
         <div className="module-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-x-5 gap-y-5">
           {[...Array(68)].map((item) => (
--- src/pages/dashboard/courses/components/MyCourses.jsx
+++ src/pages/dashboard/courses/components/MyCourses.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { SectionTitleDashboard } from "../../../../components/typography/Typography";
 import { RecentCourseCard } from "../../../../components/card/CourseCard";
+import { ButtonPrimary } from "../../../../components/button/Button";
 
 const MyCourses = () => {
   return (
     <div>
       <div className="">
-        <SectionTitleDashboard>My courses</SectionTitleDashboard>
+        <div className="flex items-center justify-between">
+          <SectionTitleDashboard>My courses</SectionTitleDashboard>
+          <ButtonPrimary>Create new course</ButtonPrimary>
+        </div>
         <div className="course-list mt-content grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-4">
           <RecentCourseCard />
           <RecentCourseCard />
